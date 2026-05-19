5e859123c099d85d74ceccbea47e68dec281ede0
Sankar0805 <roalityzoro@gmail.com>
2026-05-19 10:56:18 +0530

    Update urls.py

--- djangoand/djangoand_project/urls.py
+++ djangoand/djangoand_project/urls.py
@@ -5,7 +5,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chat.urls')),
-    # Serve React frontend for all other routes (after building)
     path('', TemplateView.as_view(template_name='index.html')),
     path('<path:path>', TemplateView.as_view(template_name='index.html')),
 ]
